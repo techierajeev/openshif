33925ed72f6da02204791484d62bbc4376ed5a92
Rajeev Ranjan <rajeev.ranjan@DC2-DEV163.local>
2019-02-23 22:17:31 +0530

    added php file

---
+++ php/myapp/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Openshift Exam</title>
+    </head>
+    <body>
+        <p>
+            Hello Welcome from Adhoc Networks. Today is <?= date('l \t\h\e jS') ?>.
+        </p>
+    </body>
+</html>
+
